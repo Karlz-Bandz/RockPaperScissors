8a9e61baff5fd624c4e0148bbbf2a348bc52d743
Melak <km250542@ncr.com>
2023-03-15 10:38:05 +0100

    Initial Commit

---
+++ src/main/java/RockPaperScissors.java
@@ -0,0 +1,12 @@
+public class RockPaperScissors {
+    static boolean printenL(int number){
+        System.out.println(number + "L");
+
+        return true;
+    }
+    public static void main(String[] args) {
+
+        printenL(340);
+
+    }
+}
---
+++ src/test/java/RockPaperScissorsTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class RockPaperScissorsTest {
+
+    private RockPaperScissors rockPaperScissorsTest;
+
+    RockPaperScissorsTest(){
+       rockPaperScissorsTest = new RockPaperScissors();
+    }
+
+    @Test
+    public void PrintenL_Test(){
+        boolean tester = rockPaperScissorsTest.printenL(340);
+
+        Assertions.assertEquals(tester, true);
+    }
+}
