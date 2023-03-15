753096992d7a97ccd1d1d6ca97de08125f7a22e5
Melak <km250542@ncr.com>
2023-03-15 15:22:19 +0100

    Game Board ready

--- src/main/java/RockPaperScissors.java
+++ src/main/java/RockPaperScissors.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class RockPaperScissors {
     static boolean printenL(int number){
         System.out.println(number + "L");
@@ -6,7 +8,19 @@ public class RockPaperScissors {
     }
     public static void main(String[] args) {
 
-        printenL(340);
+        int playerPoints = 0;
+        int pcPoints = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello, welcome to \033[31mROCK \033[0mPAPER \033[34mSCISSORS \033[0m");
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("\033[31mPress R to ROCK             \033[35mPLAYER: " + playerPoints);
+            System.out.println("\033[0mPress P to PAPER");
+            System.out.println("\033[34mPress S to CUTTTT.....          \033[36mPC: " + pcPoints + "\033[0m");
+        }
+
 
     }
 }
