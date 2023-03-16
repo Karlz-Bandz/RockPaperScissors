ba8d14ec706985c7672068f9edfaf2fdedbcd7a4
Melak <km250542@ncr.com>
2023-03-16 08:20:29 +0100

    Beta version

--- src/main/java/set/GameSet.java
+++ src/main/java/set/GameSet.java
@@ -2,74 +2,124 @@ package set;
 
 import interfaces.Settingz;
 import java.util.Random;
+import java.util.Scanner;
 
 /*
 * This class helps manage the all game logic
  */
-
 public class GameSet implements Settingz {
 
+  private Scanner scanner = new Scanner(System.in);
   private int playerPoints = 0;
   private int pcPoints = 0;
   private Random rand = new Random();
 
-  public int getPlayerPoints() {
+  private int getPlayerPoints() {
     return playerPoints;
   }
 
-  public int getPcPoints() {
+  private int getPcPoints() {
     return pcPoints;
   }
 
-    @Override
-    public boolean checkState(char playerChose)
-    {
-      int playerIntChose = 0;
-      int pcIntChose = rand.nextInt(3);
+  private void setPlayerPoints(int points)
+  {
+    this.playerPoints = points;
+  }
+  private void setPcPoints(int points)
+  {
+    this.pcPoints = points;
+  }
 
-      if(Character.toUpperCase(playerChose) == 'R')
-        playerIntChose = 0;
-      else if(Character.toUpperCase(playerChose) == 'P')
-        playerIntChose = 1;
-      else if(Character.toUpperCase(playerChose) == 'S')
-        playerIntChose = 2;
 
-      System.out.println(pcIntChose);
-      System.out.println(playerIntChose);
 
-      if(playerIntChose == pcIntChose)
-        return false;
 
-      if(playerIntChose == 0 && pcIntChose == 1)
-      {
-        ++pcPoints;
-        return true;
-      }else if(playerIntChose == 0 && pcIntChose == 2)
-      {
-        ++playerPoints;
-        return true;
-      }else if(playerIntChose == 1 && pcIntChose == 0)
-      {
-        ++playerPoints;
-        return true;
-      }else if(playerIntChose == 1 && pcIntChose == 2)
-      {
-        ++pcPoints;
-        return true;
-      }else if(playerIntChose == 2 && pcIntChose == 0)
-      {
-        ++pcPoints;
-        return true;
-      }else if(playerIntChose == 2 && pcIntChose == 1)
-      {
-        ++playerPoints;
-        return true;
-      }
-      return false;
-    }
+
+
 
   @Override
-  public boolean checkCorrect(char buchstabe)
+  public void gameBody()
+  {
+    for(int i = 0; i < 5; i++)
+    {
+      System.out.println("\033[31mPress R to ROCK             \033[35mPLAYER: " + this.playerPoints);
+      System.out.println("\033[0mPress P to PAPER");
+      System.out.println("\033[34mPress S to CUTTTT.....          \033[36mPC: " + this.pcPoints + "\033[0m");
+
+      boolean buchstabeState = false;
+      char valotileBuchstabe = 'R';
+
+      while(!buchstabeState)
+      {
+        valotileBuchstabe = scanner.next().charAt(0);
+        buchstabeState = this.checkCorrect(valotileBuchstabe);
+      }
+
+      this.checkState(valotileBuchstabe);
+
+    }
+    this.setWhoWin();
+
+  }
+  private String translationFromInt(int number)
+  {
+          if(number == 0)
+              return "\33[31mROCK\33[0m";
+          else if(number == 1)
+              return "PAPER";
+          else if(number == 2)
+             return "\33[34mSCISSORS\33[0m";
+
+          return "Errix";
+  }
+  private boolean checkState(char playerChose)
+  {
+    int playerIntChose = 0;
+    int pcIntChose = rand.nextInt(3);
+
+    if(Character.toUpperCase(playerChose) == 'R')
+      playerIntChose = 0;
+    else if(Character.toUpperCase(playerChose) == 'P')
+      playerIntChose = 1;
+    else if(Character.toUpperCase(playerChose) == 'S')
+      playerIntChose = 2;
+
+    System.out.println();
+    System.out.println("\033[36m----PC: \033[0m" + translationFromInt(pcIntChose));
+    System.out.println("\033[35mPlayer: \033[0m" + translationFromInt(playerIntChose));
+    System.out.println();
+
+    if(playerIntChose == pcIntChose)
+      return false;
+
+    if(playerIntChose == 0 && pcIntChose == 1)
+    {
+      ++pcPoints;
+      return true;
+    }else if(playerIntChose == 0 && pcIntChose == 2)
+    {
+      ++playerPoints;
+      return true;
+    }else if(playerIntChose == 1 && pcIntChose == 0)
+    {
+      ++playerPoints;
+      return true;
+    }else if(playerIntChose == 1 && pcIntChose == 2)
+    {
+      ++pcPoints;
+      return true;
+    }else if(playerIntChose == 2 && pcIntChose == 0)
+    {
+      ++pcPoints;
+      return true;
+    }else if(playerIntChose == 2 && pcIntChose == 1)
+    {
+      ++playerPoints;
+      return true;
+    }
+    return false;
+  }
+  private boolean checkCorrect(char buchstabe)
   {
     if(Character.toUpperCase(buchstabe) == 'P')
       return true;
@@ -79,5 +129,29 @@ public class GameSet implements Settingz {
       return true;
 
     return false;
+  }
+  private void setWhoWin() {
+    if(this.pcPoints > this.playerPoints)
+    {
+      System.out.println("\033[36mPC WON THE GAME");
+      System.out.println("PLEASE CLICK ANY KEY...\033[0m");
+      scanner.nextLine();
+      scanner.nextLine();
+    }else if(this.pcPoints < this.playerPoints)
+    {
+      System.out.println("\033[35mPLAYER WON THE GAME");
+      System.out.println("PLEASE CLICK ANY KEY...\033[0m");
+      scanner.nextLine();
+      scanner.nextLine();
+    }else if(this.pcPoints == this.playerPoints)
+    {
+      System.out.println("\033[31mNoONE WON THE GAME");
+      System.out.println("PLEASE CLICK ANY KEY TO PLAY AGAIN...\033[0m");
+      scanner.nextLine();
+      scanner.nextLine();
+      this.pcPoints = 0;
+      this.playerPoints = 0;
+      this.gameBody();
+    }
   }
 }
